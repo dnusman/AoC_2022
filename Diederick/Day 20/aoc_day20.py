558657e4b3dc95653120137a1a65608f861c75ca
dnusman@gmail.com <dnusman@gmail.com>
2023-01-03 22:08:02 +0000

    finish day 20

--- Diederick/Day 20/aoc_day20.py
+++ Diederick/Day 20/aoc_day20.py
@@ -1,82 +1,50 @@
 # Databricks notebook source
-with open('input20_small.txt') as f:
-  data = f.read().splitlines()
+from collections import deque
+import requests 
 
-original = []
-for i,v in enumerate(data):
-  original.append([int(v),i])
+small = False
+day = 20
 
-original
-
-# COMMAND ----------
-
-altered = original.copy()
-
-for i,v in enumerate(original):
-#   print(i)
-  
-  indices =list(range(len(original)))
-  indices.remove(i)
-  
-  altered[i][1] += altered[i][0]
-  
-  for other in indices:
-    if altered[i][0] > 0:
-      altered[other][1] -= 1
-    elif altered[i][0] <0:
-      altered[other][1] += 1
-
-print(altered)
-  
-  
-
-  
+if small:
+  with open("input20_small.txt") as f:
+    data = [int(x) for x in f.read().splitlines()]
+else:
+  cookies = {'session': '53616c7465645f5f0e0be1a0761339e0465a495ff5069c3d6a10a11c0caac7cbc6403ee58544f377cea286763e468e79b8742f07a024cd201ef42047fbe56d2d'}
+  r = requests.get('https://adventofcode.com/2022/day/{0}/input'.format(day), cookies=cookies)
+  data = [int(x) for x in r.text.splitlines()]
 
 
 # COMMAND ----------
 
-altered = original.copy()
-
-for i,v
-
-
-
-
-# COMMAND ----------
-
-def update_position(current_position,increment,length):
-  if increment < 0:
-    new_position = current_position + increment
-    if new_position < 0:
-      new_position = length-abs(new_position)
-  else:
-    new_position = current_position + increment
-    if new_position > length:
-      new_position = new_position - length
-  return new_position
-
-
-update_position(0,-1,7)
-
-# COMMAND ----------
-
-#rules
-
-#1. new pos = 0:
-# list = list + key
-
-#2. new pos <0:
+def mix(ind,enumerated):
+  while enumerated[0][0] != ind:
+    enumerated.rotate(-1)
   
-#   left = length - value under zero
-#   right = list[-value under zero:]
-#   left + key + right
-#3. new pos > length:
-
-
-for i in original:
+  current_pair = enumerated.popleft()
+  shift_by = current_pair[1]
+  enumerated.rotate(-shift_by)
+  enumerated.append(current_pair)
   
+  return enumerated
+
+def value_at_n(values,n):
+  digit = (values.index(0)+n) % len(values)
+  return values[digit]
 
 # COMMAND ----------
 
-for i in range(6,2,-1):
-  print(i)
+rounds = [[1,1,1],[2,10,811589153]] #part / number of rounds / multiplication key
+
+for round in rounds:
+  new_data = [val*round[2] for val in data]
+  enumerated = deque(list(enumerate(new_data.copy())))
+  
+  for _ in range(round[1]):
+    for i in range(len(enumerated)):
+      enumerated = mix(i,enumerated)
+
+  ans = 0
+  for n in [1000,2000,3000]:
+    ans += value_at_n([val[1] for val in enumerated],n)
+
+  print(f'Answer {round[0]}: {ans}')    
