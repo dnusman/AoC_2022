c7d4ab87d255b4e04f4c447ade538ad5618cf795
dnusman@gmail.com <dnusman@gmail.com>
2023-01-03 14:25:07 +0000

    finish day 17 with help

--- Diederick/Day 17/aoc_day17.py
+++ Diederick/Day 17/aoc_day17.py
@@ -27,6 +27,12 @@ jets
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC 
+# MAGIC # By Diederick
+
+# COMMAND ----------
+
 def coordinates_shape(shape,highest_rock):
   if shape == "horizontal":
     coords = [[3,highest_rock+5],[4,highest_rock+5],[5,highest_rock+5],[6,highest_rock+5]]
@@ -85,8 +91,8 @@ stopped = []
 stopped = stopped + bottom
 
 for x in range(2022):
-  if x % 100 == 0:
-    stopped = stopped[-50:]
+#   if x % 100 == 0:
+#     stopped = stopped[-50:]
   if x % 10000 == 0:
     print(x)
   shape = shapes[x%5]
@@ -129,3 +135,118 @@ for rock in stopped:
 fig, ax = plt.subplots(figsize=(10,100))
 im = plt.imshow(np.flipud(cave))
 plt.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC # By Hyper Neutrino
+
+# COMMAND ----------
+
+rocks = [
+  [0,1,2,3],
+  [1, 1j, 1 + 1j, 2 + 1j, 1 + 2j],
+  [0, 1, 2, 2 + 1j, 2 + 2j],
+  [0, 1j, 2j, 3j],
+  [0,1,1j,1+1j]
+]
+
+jets_dir = [1 if x == ">" else -1 for x in jets]
+solid = {x-1j for x in range(7)}
+height = 0
+
+rc = 0
+ri = 0
+
+rock = {x+2 + (height+3) * 1j for x in rocks[ri]}
+
+
+
+while rc < 2022:
+  for jet in jets_dir:
+    moved = {x+jet for x in rock}
+    if all(0 <= x.real < 7 for x in moved) and not (moved & solid):
+      rock = moved
+    moved = {x-1j for x in rock}
+    if moved & solid:
+      solid |= rock
+      rc += 1
+      o = height
+      height = max(x.imag for x in solid)+1
+      if rc >= 2022:
+        break
+      ri = (ri+1)%5      
+      rock = {x+2 + (height+3) * 1j for x in rocks[ri]}
+    else:
+      rock = moved
+
+print(f'Answer 1:{int(height)}')
+
+# COMMAND ----------
+
+rocks = [
+  [0,1,2,3],
+  [1, 1j, 1 + 1j, 2 + 1j, 1 + 2j],
+  [0, 1, 2, 2 + 1j, 2 + 2j],
+  [0, 1j, 2j, 3j],
+  [0,1,1j,1+1j]
+]
+
+jets_dir = [1 if x == ">" else -1 for x in jets]
+solid = {x-1j for x in range(7)}
+height = 0
+
+seen = {}
+
+def summarize():
+  o = [-20]*7
+  
+  for x in solid:
+    r = int(x.real)
+    i = int(x.imag)
+    o[r] = max(o[r],i)
+    
+  top = max(o)
+  return tuple(x - top for x in o)
+
+ 
+rc = 0
+ri = 0
+
+rock = {x+2 + (height+3) * 1j for x in rocks[ri]}
+
+T = 1000000000000
+while rc < T:
+  for ji,jet in enumerate(jets_dir):
+    moved = {x+jet for x in rock}
+    if all(0 <= x.real < 7 for x in moved) and not (moved & solid):
+      rock = moved
+    moved = {x-1j for x in rock}
+    if moved & solid:
+      solid |= rock
+      rc += 1
+      o = height
+      height = max(x.imag for x in solid)+1
+      if rc >= T:
+        break
+      ri = (ri+1)%5      
+      rock = {x+2 + (height+3) * 1j for x in rocks[ri]}
+      key = (ji,ri,summarize())
+      if key in seen:
+        lrc,lh = seen[key]
+        rem = T - rc
+        rep = rem // (rc-lrc)
+        offset = rep * (height-lh)
+        rc += rep * (rc - lrc)
+        seen = {}
+      seen[key] = (rc,height)
+    else:
+      rock = moved
+
+print(f'Answer 2: {int(height+offset)}')
+
+
+# COMMAND ----------
+
+
